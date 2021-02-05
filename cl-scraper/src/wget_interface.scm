843661daf43f72d36ecc0e84fecf91e04ae2df6a
Javier Velez <velejester@gmail.com>
2021-02-05 10:42:28 -0500

    Wget interface for recursive gets.
    Fixed some bugs in low-level parsing.
    Also extracting ERROR messages.

--- cl-scraper/src/wget_interface.scm
+++ cl-scraper/src/wget_interface.scm
@@ -36,51 +36,85 @@
   ;;
   ;; parse out hte request lines from the line port 
   (define (%parse-wget-request-output line-port)
-    (let ((lines '()))
-      (do ((line (line-input-port/read-line line-port)
-		 (line-input-port/read-line line-port)))
-	  ((or (eof-object? line)
-	       (string-prefix? "--" line))
-	   (if (eof-object? line)
-	       (reverse lines)
-	       (reverse (cons line lines))))
-	(set! lines (cons line lines)))))
-    
+    (let ((result
+	   (let ((lines (list (line-input-port/read-line line-port))))
+	     (do ((line (line-input-port/read-line line-port)
+			(line-input-port/read-line line-port)))
+		 ((or (eof-object? line)
+		      (string-prefix? "--" line))
+		  (if (eof-object? line)
+		      (reverse lines)
+		      (begin
+			(line-input-port/unread-line line-port)
+			(reverse lines))))
+	       (set! lines (cons line lines))))))
+      result))
 
   ;;
   ;; Parse out the liens from  the request into an alist structure
   (define (%parse-request-lines lines)
-    (let ((matches (irregex-match "^--(\\d+)-(\\d+)-(\\d+)[^-]+--\\s+(.*)\s*$" (first lines))))
-      (if (not matches)
-	  (begin
-	    (let ((message
-	    	   (format (string-append
-	    		    "Unable to parse request header line."
-	    		    "line=~a")
-	    		   (first lines))))
-	      (signal (condition (list 'exn 'message message))))) 
-	    ;; (raise (condition
-	    ;; 	    (&parse-error
-	    ;; 	     (message
-	    ;; 	      (format (string-append
-	    ;; 		       "Unable to parse request header line."
-	    ;; 		       "line=~a")
-	    ;; 		      (first lines)))))))
-	  (if (not (irregex-match-valid-index? matches 4))
-	      (begin
-		(let ((message
-		       (format (string-append "Failed to parse wget request "
-					      "header line, line=~a")
-			       (first lines))))
-		  (signal (list 'exn 'message message))))
-		;; (raise (condition
-		;; 	(&parse-error
-		;; 	 (message
-		;; 	  (format (string-append "Failed to parse wget request "
-		;; 				 "header line, line=~a")
-		;; 		  (first lines)))))))
-	      (list
-	       (cons url: (irregex-match-substring matches 4)))))))
+    (let ((found-results '()))
+
+      ;; Look over all lines and extract save location(s)
+      (for-each
+       (lambda (line)
+	 (let ((matches (irregex-match "^.*Saving to: ‘(.*)’.*$" line)))
+	   (if (and matches
+		    (irregex-match-valid-index? matches 1))
+	       (begin
+		 (set! found-results
+		   (alist-cons save-filename:
+			       (irregex-match-substring matches 1)
+			       found-results))
+		 (display (format "SAVED: ~a"
+				  (irregex-match-substring matches 1)))
+		 (newline)))))
+       lines)
+
+      ;; look for errors
+      (for-each
+       (lambda (line)
+	 (let ((matches (irregex-match "^.*ERROR (.*):(.*)$" line)))
+	   (if (and matches
+		    (irregex-match-valid-index? matches 1)
+		    (irregex-match-valid-index? matches 2))
+	       (let ((code (irregex-match-substring matches 1))
+		     (message (irregex-match-substring matches 2)))
+		 (display (format "ERROR: ~a (~a)" code message))
+		 (newline)
+		 (set! found-results
+		   (alist-cons error: (list code message)
+			       found-results))))))
+       lines)
+
+      ;; Look at the header line for the url accessed
+      (let ((matches (irregex-match
+		      "^--(\\d+)-(\\d+)-(\\d+)[^-]+--\\s+(.*)\s*$"
+		      (first lines))))
+	(if (not matches)
+	    (begin
+	      (let ((message
+	    	     (format (string-append
+	    		      "Unable to parse request header line."
+	    		      "line=~a")
+	    		     (first lines))))
+		(signal (condition (list 'exn 'message message))))) 
+	    (if (not (irregex-match-valid-index? matches 4))
+		(begin
+		  (let ((message
+			 (format (string-append "Failed to parse wget request "
+						"header line, line=~a")
+				 (first lines))))
+		    (signal (list 'exn 'message message))))
+		(begin
+		  (set! found-results
+		    (alist-cons url: (irregex-match-substring matches 4)
+				found-results))
+		  (display (format "URL: ~a"
+				   (irregex-match-substring matches 4)))
+		  (newline)))))
+	
+      found-results))
 	  
 
   ;;
@@ -93,7 +127,7 @@
 		 (line-input-port/read-line line-port)))
 	  ((eof-object? line) (list
 			       (cons lines: (reverse output-lines))
-			       (cons request-urls: requested-urls)))
+			       (cons request-parameters: requested-urls)))
 	(if (string-prefix? "--" line)
 	    (begin
 	      (line-input-port/unread-line line-port)
@@ -109,6 +143,8 @@
   ;; runs wget
   (define (%call-wget-and-parse-output args)
     (let ((full-args (%wget-arguments-merge args)))
+      (display (format "WGET: ~a" full-args))
+      (newline)
       (let-values (( (proc-outport proc-inport pid proc-errport)
 		     (process* "wget" full-args)))
 	(dynamic-wind
@@ -116,6 +152,7 @@
 
 	  (lambda ()
 	    (let ((outres (%wget-output-parse proc-errport)))
+	      ;; WGET writes output to STDERR (?!)
 	      ;;(copy-port proc-errport (current-output-port))
 	      outres))
 
@@ -124,6 +161,22 @@
 	    (close-input-port proc-outport)
 	    (close-input-port proc-outport))))))
 
-  
+
+  ;;
+  ;; performs a wget recursive request and return the resulting
+  ;; files and the list of urls followed
+  (define (wget-recursive root-url depth-limit accept-regex
+			  #!optional (wait 10) )
+    (let ((args (list "--recursive"
+		      "--level" (number->string depth-limit)
+		      "--regex-type" "posix"
+		      "--accept-regex" accept-regex
+		      "--span-hosts"
+		      "--wait" (number->string wait)
+		      "--random-wait"
+		      "--trust-server-names"
+		      root-url)))
+      (let ((wget-results (%call-wget-and-parse-output args)))
+	wget-results)))
 
   )
